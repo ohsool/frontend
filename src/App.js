494e8e525f81e6a03aa0bcbb79afdfe6083b4e86
Jeongyun Jang <jjy306105@gmail.com>
2021-07-28 14:30:33 +0900

    redux폴더 async/reducer 파일 추가

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import {Route} from "react-router-dom";
 import { history } from "./redux/configureStore";
 import { ConnectedRouter } from "connected-react-router";
 
-import {Main, Test} from "./pages/indexPage";
+import {Main, Test, Board, MyPage, SignUp} from "./Pages/indexPage";
 import Header from "./Header";
-import SignUp from "./pages/SignUp";
 import BeerReview from "./beerdogam/BeerReview";
 
-import Board from "./pages/Board";
-import MyPage from "./pages/MyPage";
 
 
 function App() {
@@ -23,7 +20,7 @@ function App() {
         <Route path="/test" component={Test}/>
         <Route path="/signup" exact component={SignUp}/>
         <Route path="/beerreview" exact component={BeerReview}/>
-        <Route path="/board" component={Board}/>
+        <Route path="/board/category" component={Board}/>
         <Route path="/mypage" component={MyPage}/>   
       </ConnectedRouter>
     </React.Fragment>
@@ -35,5 +32,6 @@ export default App;
 const GlobalStyle = createGlobalStyle`
   body{
     font-family: "Noto Sans KR", sans-serif;
+    background-color: #F2F3F7;
   }
 `;
--- src/pages/indexPage.js
+++ src/pages/indexPage.js
@@ -1,4 +1,7 @@
 import Main from "./Main";
 import Test from "./Test";
+import Board from ".//Board";
+import MyPage from "./MyPage";
+import SignUp from "./SignUp";
 
-export {Main, Test};
+export {Main, Test, Board, MyPage, SignUp};
